ba24f4957e568067e5c4444249ba2906afdbcb8d
Cristian Tonato <cristianotona@gmail.com>
2022-05-30 20:09:12 +0200

    feature/commitadding

--- src/components/buttonV/buttonV.js
+++ src/components/buttonV/buttonV.js
@@ -2,7 +2,15 @@ import { PERSONS } from "../../models/person";
 import { Counter } from "..//counter/counter";
 export function ButtonV({id}){
     //console.log(id)
-
+    function handlerClick(ev,id){
+        console.log(ev)
+        PERSONS.forEach(item => {
+            if(item.id === id){
+                item.selected=true
+            }
+        })
+       
+    }
     return(
         <i className ="icon gentleman__icon fas fa-check" onClick={(ev) => Counter.handlerClick(ev, id) }></i>
     )
